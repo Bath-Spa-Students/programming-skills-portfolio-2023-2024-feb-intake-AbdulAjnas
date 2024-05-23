d3502e74655f970a87e8bdbefc3eada825c1474e
AbdulAjnas <145003746+AbdulAjnas@users.noreply.github.com>
2024-05-23 11:58:27 +0400

    Create Exercise2.py

---
+++ Chapter 2- Variables & Comments/Exercise2.py
@@ -0,0 +1,4 @@
+quote = "I've learned that people will forget what you said, people will forget what you did, but people will never forget how you made them feel."
+author = "Maya Angelou"
+
+print(f"{author} once said, \"{quote}\"")
