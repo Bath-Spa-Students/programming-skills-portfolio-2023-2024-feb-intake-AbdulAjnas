19f87ab83a709e2a21adb856c989b50ebad37aab
AbdulAjnas <145003746+AbdulAjnas@users.noreply.github.com>
2024-05-23 22:30:20 +0400

    Create Exercise5.py

---
+++ Chapter 4- Control Flow/Exercise5.py
@@ -0,0 +1,14 @@
+# Make a list of your three favorite fruits
+favorite_fruits = ["apple", "banana", "strawberry"]
+
+# Check if certain fruits are in your list and print a statement accordingly
+if "apple" in favorite_fruits:
+    print("You really like apples!")
+if "banana" in favorite_fruits:
+    print("You really like bananas!")
+if "orange" in favorite_fruits:
+    print("You really like oranges!")
+if "strawberry" in favorite_fruits:
+    print("You really like strawberries!")
+if "kiwi" in favorite_fruits:
+    print("You really like kiwis!")
