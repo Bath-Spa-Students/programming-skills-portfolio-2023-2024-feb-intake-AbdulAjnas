9e23e39a2d78af416177e382ecb5855bf0df57d5
AbdulAjnas <145003746+AbdulAjnas@users.noreply.github.com>
2024-05-23 12:07:37 +0400

    Create Exercise1.py

---
+++ Chapter 4- Control Flow/Exercise1.py
@@ -0,0 +1,6 @@
+# Define the variable alien_color and assign it a value
+alien_color = 'green'
+
+# Check if the alien's color is green
+if alien_color == 'green':
+    print("Congratulations! You just earned 5 points.")
