38ce23169e4d933b90854353ed7648694e1d5b35
AbdulAjnas <145003746+AbdulAjnas@users.noreply.github.com>
2024-05-23 11:57:45 +0400

    Create Exercise1.py

---
+++ Chapter 2- Variables & Comments/Exercise1.py
@@ -0,0 +1,11 @@
+# Print the message
+message = "Hello, this is the first message."
+
+# Print the initial message
+print(message)
+
+# Change the value of the variable to a new message
+message = "This is the new message after changing the variable."
+
+# Print the new message
+print(message)
