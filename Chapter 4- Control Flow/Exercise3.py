d649f4d21f8e74400a71d04d998aab79bf2bcfac
AbdulAjnas <145003746+AbdulAjnas@users.noreply.github.com>
2024-05-23 22:25:46 +0400

    Create Exercise3.py

---
+++ Chapter 4- Control Flow/Exercise3.py
@@ -0,0 +1,10 @@
+# Define the variable alien_color and assign it a value
+alien_color = 'green'
+
+# Check the color of the alien and print the appropriate message
+if alien_color == 'green':
+    print("Congratulations! You just earned 5 points for shooting the green alien.")
+elif alien_color == 'yellow':
+    print("Congratulations! You just earned 10 points for shooting the yellow alien.")
+else:
+    print("Congratulations! You just earned 15 points for shooting the red alien.")
