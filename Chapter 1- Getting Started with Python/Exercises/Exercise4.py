e179ca10e32309d6c7e1e8c9b3e1739dd6d5ba33
AbdulAjnas <145003746+AbdulAjnas@users.noreply.github.com>
2024-05-23 11:55:23 +0400

    Create Exercise4.py

---
+++ Chapter 1- Getting Started with Python/Exercises/Exercise4.py
@@ -0,0 +1,10 @@
+# Define three string variables
+string1 = "Hello"
+string2 = "World"
+string3 = "!"
+
+# Concatenate the strings with spaces in between
+combined_string = string1 + " " + string2 + " " + string3
+
+# Print the combined string
+print(combined_string)
