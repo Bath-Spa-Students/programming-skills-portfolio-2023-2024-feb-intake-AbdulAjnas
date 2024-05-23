e07f4f6aac62ea1764b60074ad99012846417a36
AbdulAjnas <145003746+AbdulAjnas@users.noreply.github.com>
2024-05-23 12:01:38 +0400

    Create Exercise1.py

---
+++ Chapter 3- Data Structures/Exercise1.py
@@ -0,0 +1,6 @@
+# Define a list of friends' names
+names = ["Ajnas", "Faheem", "Hamza", "Jaseem", "Jeff"]
+
+# Print each person's name by accessing each element in the list
+for name in names:
+    print(name)
