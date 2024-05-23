43edb32551553fa4877f69a3ac5a4f20464ec6d7
AbdulAjnas <145003746+AbdulAjnas@users.noreply.github.com>
2024-05-23 12:03:00 +0400

    Create Exercise3.py

---
+++ Chapter 3- Data Structures/Exercise3.py
@@ -0,0 +1,6 @@
+# Define a list of different modes of transportation
+modes_of_transportation = ["car", "bicycle", "motorcycle", "boat", "plane"]
+
+# Print statements about each mode of transportation
+for mode in modes_of_transportation:
+    print(f"I would like to own a {mode.capitalize()}.")
